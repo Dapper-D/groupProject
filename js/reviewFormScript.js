1c81794aef735c0422619d08d5ed7bc68e52966e
Fernando Henrique <ferna@MacBook-Pro.local>
2024-04-04 15:50:33 -0700

    I made a javascript file to make the succesfull message display after the required fields are completed

---
+++ js/reviewFormScript.js
@@ -0,0 +1,6 @@
+document.getElementById('reviewForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert('Your review was sent successfully'); // Display the alert message
+    // You can add code here to handle the form submission, such as sending data to a server
+    console.log('sad')
+});
