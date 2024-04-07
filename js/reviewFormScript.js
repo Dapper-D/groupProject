b5fa4fa565db8510ce06b89ec59e08b61a3d8816
Fernando Machado <fernandoh_machado@yahoo.com.br>
2024-04-06 20:48:54 -0700

    Update reviewFormScript.js

--- js/reviewFormScript.js
+++ js/reviewFormScript.js
@@ -1,6 +1,5 @@
 document.getElementById('reviewForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    alert('Your review was sent successfully'); // Display the alert message
+    event.preventDefault();
+    alert('Your review was sent successfully'); 
     // You can add code here to handle the form submission, such as sending data to a server
-    console.log('sad')
 });
